b4fc9730d5c3a7a709e9fa8d6cb7ea01f86b95a6
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-09-19 16:56:38 +0000

    Escape the maze algorithm

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
 # -------------------- Escape the Maze -------------------- #
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# Hurdle3
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump()
+
+# Hurdle 4
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# ESCAPE THE MAZE #
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
-def jump():
-    turn_left()
+
+while front_is_clear():
     move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
+turn_left()
+    
 while not at_goal():
-    if front_is_clear():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
         move()
     else:
-        jump()
-
-#while not at_goal(
+        turn_left()
